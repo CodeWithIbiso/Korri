83aa3360e06891a3a44ffad9c568b72cc806f2a8
Ibiso Bob-manuel <ibiso.dennie@gmail.com>
2023-01-07 19:52:33 +0100

    removed sensitive code

--- server/index.js
+++ server/index.js
@@ -6,21 +6,16 @@ import propertyRoutes from './routes/properties.js'
 import userRoutes from './routes/user.js' 
 import dotenv from 'dotenv'
 
-// const CONNECTION_URL = 'mongodb+srv://dennie:dennie@cluster0.kfsfa.mongodb.net/?retryWrites=true&w=majority'
 dotenv.config()
 const app = express()
-const CONNECTION_URL = process.env.CONNECTION_URL// 'mongodb+srv://dennie:5zjBuRCunQsoa2kn@cluster0.kfsfa.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL 
 const PORT = process.env.PORT || 8000
  
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>app.listen(PORT,()=>console.log(`Server running on port ${PORT}`)))
 .catch((error)=>console.log("error",error)) ;
   
-// mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-// .then(()=>app.listen(PORT,()=>console.log(`Server running on port ${PORT}`)))
-// .catch((error)=>console.log("error",PORT,error)) ;
-// Are we not going to close connection to database after every request?
-
+ 
 
 app.use(bodyParser.json({limit:'50mb',extended:true}))
 app.use(bodyParser.urlencoded({limit:'50mb',extended:true}))
